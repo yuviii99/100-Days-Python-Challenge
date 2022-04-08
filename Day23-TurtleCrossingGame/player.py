c849876990bfe3e78c6f8f398e2341d851ef0f52
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2022-04-08 17:05:46 +0530

    Build Turtle Crossing Game

---
+++ Day23-TurtleCrossingGame/player.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+
+STARTING_POSITION = (0, -250)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
---
+++ Day23-TurtleCrossingGame/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.update_level()
+
+    def update_level(self):
+        self.goto(-280, 250)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.clear()
+        self.update_level()
