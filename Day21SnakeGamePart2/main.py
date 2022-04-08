ac39fb6fbd07dfe59f36cb3e649b39e418e661ce
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2022-04-09 03:34:24 +0530

    Modified Snake Game to record high score

--- Day21SnakeGamePart2/main.py
+++ Day21SnakeGamePart2/main.py
@@ -26,21 +26,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    # Detect collision with snake
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.score_update()
+        scoreboard.increase_score()
 
     # Detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
