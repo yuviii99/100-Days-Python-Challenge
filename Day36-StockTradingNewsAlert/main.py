ca4a6be6b73c0fd1dc2fe3d32d7f83aef48d896d
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2022-04-27 23:32:41 +0530

    Completed Stock Trading News App

--- Day36-StockTradingNewsAlert/main.py
+++ Day36-StockTradingNewsAlert/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -6,8 +7,11 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_PRICE_API = "4GTDUPDWAII85M6Y"
-NEWS_API_KEY = "88caccdda52746a38028ad11b1d335bf"
+STOCK_PRICE_API = "ALPHAVANTAGE API KEY"
+NEWS_API_KEY = "NEWS API KEY"
+TWILIO_SID = "TWILIO SID"
+TWILIO_AUTH_TOKEN = "AUTH_TOKEN"
+TWILIO_PHONE = "TWILIO_PHONE"
 
 stock_price_params = {
     "function": "TIME_SERIES_DAILY",
@@ -24,35 +28,33 @@ list_daily_stock_price = list(daily_stock_price.values())
 yesterday_close = float(list_daily_stock_price[0]["4. close"])
 day_before_yesterday_close = float(list_daily_stock_price[1]["4. close"])
 
-price_difference = abs(yesterday_close - day_before_yesterday_close)
+price_difference = yesterday_close - day_before_yesterday_close
+if price_difference > 0:
+    upDown = "🔺"
+else:
+    upDown = "🔻"
+price_difference = abs(price_difference)
 
-diff_percentage = (price_difference / yesterday_close) * 100
-print(diff_percentage)
+diff_percentage = round((price_difference / yesterday_close) * 100)
 
-if diff_percentage > 5:
+if True:
     news_params = {
         "qInTitle": COMPANY_NAME,
         "apikey": NEWS_API_KEY
     }
     news_articles = requests.get(url=NEWS_ENDPOINT, params=news_params)
     articles = news_articles.json()["articles"]
-    print(articles)
-    top_articles = articles[0:3]
+    top_articles = articles[:3]
 
-
-
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# Send a separate message with each article's title and description to your phone number. 
-# HINT 1: Consider using a List Comprehension.
-
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    formatted_messages = [
+        f"{COMPANY_NAME}: {upDown}{diff_percentage}%\nHeadline: {article['title']}. \nBrief: {article['description']}"
+        for article in top_articles]
+    # Sending SMS
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for article in formatted_messages:
+        message = client.messages \
+            .create(
+            body=article,
+            from_=TWILIO_PHONE,
+            to="TO PHONE NUMBER"
+        )
