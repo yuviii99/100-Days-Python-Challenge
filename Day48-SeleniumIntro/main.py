7c92848e1caf1110f8e237dac112ce2ddc431da1
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2022-05-10 00:16:58 +0530

    Selenium Challenge

--- Day48-SeleniumIntro/main.py
+++ Day48-SeleniumIntro/main.py
@@ -5,9 +5,16 @@ from selenium.webdriver.common.by import By
 chrome_driver_path = "/Users/yuvi/Development/chromedriver"
 driver = webdriver.Chrome(service=Service(chrome_driver_path))
 
+driver.get("https://www.python.org/")
+event_times = driver.find_elements(By.CSS_SELECTOR, "div.event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, "div.event-widget li a")
+events = {}
 
-driver.get("https://www.amazon.in/Apple-iPhone-13-128GB-Midnight/dp/B09G9HD6PD/ref=sr_1_1_sspa?crid=4JKKU9R2XF1B&keywords=iphone%2B13&qid=1652030188&sprefix=iphone%2B1%2Caps%2C232&sr=8-1-spons&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVUNJOEtER0VWRFJHJmVuY3J5cHRlZElkPUEwMDg1MDgzMk8xQk1TSTNST1lGTSZlbmNyeXB0ZWRBZElkPUEwMzMzNDc2Q0NKRDQ2R1IwM0ozJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1")
-price = driver.find_element(By.CSS_SELECTOR, "span.apexPriceToPay")
-print(price.text)
+for n in range(len(event_times)):
+    events[n] = {
+        'time': event_times[n].text,
+        'name': event_names[n].text
+    }
 
+print(events)
 driver.quit()
